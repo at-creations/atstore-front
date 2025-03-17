3f82b33ef13d92c555ac72856fdc35d32db1d55e
Bao Nguyen <pbnguyen@ualberta.ca>
2025-03-17 14:05:22 -0600

    Add BackgroundDecoratives component for enhanced visual aesthetics; update Home page layout for improved section organization

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,14 +2,28 @@ import { Hero } from "@/app/components/Hero";
 import { AboutUs } from "@/app/components/AboutUs";
 import { FeaturedProducts } from "@/app/components/FeaturedProducts";
 import { ContactUs } from "@/app/components/ContactUs";
+import { BackgroundDecoratives } from "@/app/components/ui/BackgroundDecoratives";
 
 export default function Home() {
   return (
-    <div className="mx-auto">
+    <div className="mx-auto relative overflow-hidden">
       <Hero />
-      <FeaturedProducts />
-      <AboutUs />
-      <ContactUs />
+      <div id="home-sections" className="space-y-24 relative">
+        {/* Background decoratives that add aesthetic appeal */}
+        <BackgroundDecoratives />
+
+        <div className="relative">
+          <FeaturedProducts />
+        </div>
+
+        <div className="relative">
+          <AboutUs />
+        </div>
+
+        <div className="relative">
+          <ContactUs />
+        </div>
+      </div>
     </div>
   );
 }
